5dd0147f56d8d65e396a80a3705476fff9828b93
Jonas Savickas <jonsavickas@gmail.com>
2024-12-02 18:54:55 +0200

    split

--- war/game.py
+++ war/game.py
@@ -5,25 +5,43 @@ class Deck:
         self.ranks = ["hearts", "spades", "diamonds", "clubs"]
         self.suits = ["2","3","4","5","6","7","8","9","10","jack","queen","king","ace"]
         self.cards = []
-
         for suit in self.suits:
             for rank in self.ranks:
                 self.cards.append([rank, suit])
-                # print(self.cards)
-        # for card in self.cards:
+        self.values = {
+            "2":2, "3":3, "4":4, "5":5, "6":6, "7":7,
+            "8":8, "9":9, "10":10, "jack":10,
+            "queen":10, "king":10, "ace":10
+            }
+    def get_card_value(self, card):
+        rank = card[0]
+        return self.values[rank]
+
     def shuffle(self):
         random.shuffle(self.cards)
-        # print("shuffled stuff", self.cards)
         return self.cards
+    
+    def __str__(self):
+        return (f"{self.cards}")
+    
+    def split(self):
+        lenght = len(self.cards)
+        middle = lenght // 2 
+        player_half = self.cards[:middle]
+        computer_half = self.cards[middle:]
+        return player_half, computer_half
 
 
 
-# def main():
-#     deck = Deck()
-#     print(deck)
-#     deck.shuffle()
-# if __name__ == "__main__":
-#     main()
+def main():
+    deck = Deck()
+    deck.shuffle()
+    # print(deck)
+    player_half, computer_half = deck.split()
+    print(f"Player : {player_half}")
+    print(f"Computer : {computer_half}")
+if __name__ == "__main__":
+    main()
 
 
 # class Deck:
